992c8941367a7962f9b36a3139be45e648c119d1
Abrshhh <abreham.dessiem@gmail.com>
2022-06-05 11:50:22 -0700

    kfifj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-int main() 
-{
-puts("Programming is like building a multilingual puzzle\n");
-return (0);
+int main()  {
+
+	puts("Programming is like building a multilingual puzzle\n");
+
+return 0;
+
 }
