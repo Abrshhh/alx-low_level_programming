2fc5fbb0c32300d527fae925e5e6591767683f7b
Abrshhh <abreham.dessiem@gmail.com>
2022-06-06 13:14:36 -0700

    khgg

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point 
+ * main - Entry point
  * Description: print base 16 numbers
  * Return: Always 0 (success)
  */
@@ -15,7 +15,7 @@ int main(void)
 		putchar (d);
 		d++;
 	}
-	for (c = 'a'; c<= 'f'; c++)
+	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}
