be99b3c7c41fd04ddf7f90b5e1884f8b8c465624
Abrshhh <abreham.dessiem@gmail.com>
2022-06-06 14:57:27 -0700

    llk

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+
+/**
+ * main - string printing
+ * Description: using printf function
+ * Return: 0
+ */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 
