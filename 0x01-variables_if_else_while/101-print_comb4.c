130f1c236be6c32408bc5df7432aa3c429279f24
Abrshhh <abreham.dessiem@gmail.com>
2022-06-06 14:16:03 -0700

    lkjj

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,18 +10,17 @@ int main(void)
 	int c;
 	int i;
 	int k;
-	for (c ='0'; c <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
 		{
 			for (k = '0'; k <= '9'; k++)
 			{
-				if (c < i && i< k)
+				if (c < i && i < k)
 				{
 					putchar(c);
 					putchar(i);
 					putchar(k);
-					
 					if (c != '7')
 					{
 						putchar(',');
