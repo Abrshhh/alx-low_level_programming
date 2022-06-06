007b16c871182aa58a3ac52ca88193cd72626b36
Abrshhh <abreham.dessiem@gmail.com>
2022-06-06 14:52:20 -0700

    kkk

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-int maiin(void)  
+
+/**
+ * main - Entry
+ * Description: print text
+ * Return: 0
+ */
+
+int main(void)  
 {
-
-	puts("\"Programming is like building a multilingual puzzle\n");
-
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
-
 }
