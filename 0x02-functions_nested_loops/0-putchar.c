9f95bb15c100deff64f6ff788fe3676612e4d5ba
Abrshhh <abreham.dessiem@gmail.com>
2022-06-08 12:24:17 -0700

    iuu

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	char str[] = "_putchar\n";
 	int x = 0;
-	
-	while (str[x] != '\0')
+		while (str[x] != '0')
 
 			{
 			char c = str[x];
 			putchar(c);
+
 			x++;
 			}
 	return (0);
