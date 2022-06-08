34eb55cf75eb5443e9e0bc6b3d28ba8e8aae0c02
Abrshhh <abreham.dessiem@gmail.com>
2022-06-08 10:39:06 -0700

    ppp

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print alphabet -entry
+ * Description: print alphabet in lowercase
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter = "a";
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+}
