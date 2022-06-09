7b78d3a739d2642aa506251c07a23ec3c04ebd0c
Abrshhh <abreham.dessiem@gmail.com>
2022-06-09 14:26:41 -0700

    ddd

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,8 +7,7 @@
 int main(void)
 {
 	int i = 1;
-
-	while (i <= 100)
+while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
@@ -30,8 +29,7 @@ int main(void)
 		{
 			putchar(' ');
 		}
-
-		i++;
+i++;
 	}
 	putchar('\n');
 	return (0);
