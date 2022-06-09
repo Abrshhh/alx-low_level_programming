7473d1ee06316222a10178f3cb5360e392f5edbb
Abrshhh <abreham.dessiem@gmail.com>
2022-06-09 14:02:02 -0700

    iiuu

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - dig
+ * @n: print _
+ * Return: void
+ */
+ void print_square(int n)
+{
+	int i = 0, ii;
+
+	while (i < n && n > 0)
+	{
+		ii = 0;
+		while (ii < n)
+		{
+			putchar('#');
+			ii++;
+		}
+		putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		putchar('\n');
+}	
