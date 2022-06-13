62b9f3396fc1fbb7f6048c5c3c891e0f7041edb4
Abrshhh <abreham.dessiem@gmail.com>
2022-06-13 13:09:45 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - reverse
+ * @s: ref
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while(i--)
+		putchar(s[i]);
+	putchar('\n');
+}
