1f14939a280955b70322b4aba159d8ce267c17ba
Abrshhh <abreham.dessiem@gmail.com>
2022-06-13 13:46:17 -0700

    hhh

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts_half - 2nd half
+ * @str: str ref
+ * return: 0
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
