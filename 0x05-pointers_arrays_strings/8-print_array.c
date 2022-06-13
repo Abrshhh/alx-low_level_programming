764123c31baf19943f3216b3f834263a95c72d9a
Abrshhh <abreham.dessiem@gmail.com>
2022-06-13 14:09:00 -0700

    mmm

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - print
+ * @a: array
+ * @n: int
+ * Description: comma and spp
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	i = 0;
+	for (n--; n >= 0; n--, i++)
+	{
+		printf("%d", a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
