06504e0977953295946db241ac71063e08a17a8f
Abrshhh <abreham.dessiem@gmail.com>
2022-06-28 12:23:27 -0700

    lll

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - free
+ * @grid: 2d
+ * @height: heig grid
+ */
+void free_grid(int **grid, int height)
+{
+	int index;
+	
+	for (index = 0; index < height; index++)
+		free(grid[index]);
+	free(grid);
+}
