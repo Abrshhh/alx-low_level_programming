0bbc5d4083d506f24cb2e88acaea7adb7dd39b14
Abrshhh <abreham.dessiem@gmail.com>
2023-08-02 13:28:52 -0700

    edited 3

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,8 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 int _strlen_recursion(char *s);
 {
 if (*s == '\0')
 	return (0);
+return (1 + _strlen_recursion(s +1));
 }
-	return (1 + _strlen_recursion(s +1));
