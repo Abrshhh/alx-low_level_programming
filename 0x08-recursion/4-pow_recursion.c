46ef41ea241894ab27b313b297ad1726a0a42191
Abrshhh <abreham.dessiem@gmail.com>
2023-08-02 14:44:28 -0700

    edited 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include <math.h>
 int _pow_recursion(int x, int y)
+int main ()
 {
 	if (y < 0)
 		return (-1);
-	return (x * _pow_recursion(x, y-1));
+	return (x^y);
 }
