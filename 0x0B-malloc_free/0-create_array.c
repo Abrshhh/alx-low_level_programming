eff8a4e91d30b72773ab65258c2f1fb0e71d5db4
Abrshhh <abreham.dessiem@gmail.com>
2023-08-08 13:57:45 -0700

    edited

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,18 +3,24 @@
 /**
  * @size: size of arrays
  * @c: initialized char
- * @create array: creates an array of chars, and initializes it with a specific char
+ * @create array: creates an array of chars,
+ * and initializes it with a specific char
  * Return: pointer to array if success, NULL if fail
  */
 char *create_array(unsigned int size, char c)
+
 {
 	char *pointer;
 	unsigned int integer = 0;
 	if (size == 0)
+	{
 		return NULL;
+	}
 	pointer = (char *) malloc(sizeof(char) * size);
 	if (pointer == NULL)
+	{
 		return (0);
+	}
 	while (integer < size)
 	{
 		*(pointer + integer) = c;
