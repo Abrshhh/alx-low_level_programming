0b4d37d8b7c377dd5189cd122319a46a5942678e
Abrshhh <abreham.dessiem@gmail.com>
2023-08-09 11:51:36 -0700

    edited

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /*
- * main : prints all function it recieves
+ * main - prints all function it recieves
  * @argc: prints the number of arguments
  * argv: prints string of arguments
- * return: always 0 (completed)
+ * Return: always 0 (completed)
  */
-int _atoi(char *s)
 int main(int argc, char *argv[])
 {
 	int product = 0;
-	product = _atoi(argv[1]) * _atoi(argv[2]);
+	
+	product = atoi(argv[1]) * atoi(argv[2]);
+	
 	if (argc == 3)
 	{
 	printf("%d\n", product);
-
 	return (0);
 	}
+	
 	else
 	{
-
 	printf("Error\n");
 	return (1);
 	}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -
+ * @argc:
+ * @argv:
+ *
+ * Return:
+ */
+int main(int argc, char *argv)
+	int i;
+	int sum;
+	
