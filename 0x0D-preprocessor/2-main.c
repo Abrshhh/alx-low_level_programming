cde1ec5d33fc7531d4814c559207566a86ff3f85
Abrshhh <abreham.dessiem@gmail.com>
2023-08-14 12:57:59 -0700

    task 0

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+#define printf("%s, __FILE__")
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,2 @@
 #include <stdio.h>
-#define ABS(x) if (x >= 0) \
-	printf(%d, x); \
-	else \
-	printf(%d, -1 * x);
+#define ABS(x) 
