1de4e4cdd108f95ffb7ab5d6b8ae2a35c5b66e95
Abrshhh <abreham.dessiem@gmail.com>
2023-08-17 16:36:19 -0700

    ttask 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,9 +14,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		if (!separator)
-			printf("%d\n", va_arg(list, int));
+			printf("%d", va_arg(list, int));
 		else if (separator && i == 0)
-			printf("%d\n", va_arg(list, int));
+			printf("%d", va_arg(list, int));
 		else
 			printf("%s%d", separator, va_arg(list, int));
 	}
