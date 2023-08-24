3cec5419f39688c7c9dd595cd1d2c180768d98d5
Abrshhh <abreham.dessiem@gmail.com>
2023-08-24 14:48:24 -0700

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,18 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/**
+ *
+ */
+
+typedef struct list_x
+{
+	char *str;
+	unsigned int len;
+	struct list_x *next;
+}list_t;
+
 size_t print_list(const list_t *h);
-
-
 
 
 #endif
