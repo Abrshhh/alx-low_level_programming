29d3bf0546bcb4ad1a095928f271849f646ca141
Abrshhh <abreham.dessiem@gmail.com>
2023-09-30 13:02:01 -0700

    task 2 edited

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,20 @@
 
 void print_alphabet_x10(void)
 {
+	char ch;
 	int i;
-	for (i = 0; i<=10; i++)
-		print_alphabet();
+
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
